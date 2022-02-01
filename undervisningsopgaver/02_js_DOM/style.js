26bce911a2adec2710cd43ccacd26edbb0f1b510
”Christopher <chri26kx@stud.kea.dkgit config --global user.email chri26kx@stud.kea.dk>
2022-02-01 11:29:40 +0100

    part 2

---
+++ undervisningsopgaver/02_js_DOM/style.js
@@ -0,0 +1,17 @@
+const h1 = document.querySelector("h1");
+console.log(h1);
+
+const h2 = document.querySelector("h2");
+console.log(h2);
+
+const h3 = document.querySelector("h3");
+console.log(h3);
+
+const p = document.querySelector("p");
+console.log(p);
+
+const img1 = document.querySelector(".img1");
+console.log(img1);
+
+const img2 = document.querySelector(".img2");
+console.log(img2);

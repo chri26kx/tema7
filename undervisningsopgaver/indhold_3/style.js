ddc831f9e532b1fdb8d287c630c326cbe4d7c96c
”Christopher <chri26kx@stud.kea.dkgit config --global user.email chri26kx@stud.kea.dk>
2022-02-02 13:53:24 +0100

    Update

---
+++ undervisningsopgaver/indhold_3/style.js
@@ -0,0 +1,33 @@
+const h1 = document.querySelector("h1");
+console.log(h1);
+
+const h2 = document.querySelector("h2");
+console.log(h2);
+
+const h3_1 = document.querySelector("article:nth-child(2) h3");
+console.log(h3_1);
+
+const h3_2 = document.querySelector("article:nth-child(3) h3");
+console.log(h3_2);
+
+const img1 = document.querySelector("article:nth-child(2) img");
+console.log(img1);
+
+const img2 = document.querySelector("article:nth-child(3) img");
+console.log(img2);
+
+const article1 = document.querySelector("article:nth-child(2)");
+console.log(article1);
+
+const article2 = document.querySelector("article:nth-child(3)");
+console.log(article2);
+
+h1.textContent = "HELLO";
+h2.textContent = "Med dig";
+h3_1.textContent = "gudrun";
+h3_2.textContent = "Feta";
+
+img1.src = "https://placeimg.com/700/500/nature";
+img2.src = "https://placeimg.com/700/500/nature";
+img1.alt = "natur";
+img2.alt = "natur";
